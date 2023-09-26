1ac7fd8c57d0fb9a352d37864242a68961bf90ef
makenchi <lucas.santos9322@gmail.com>
2023-09-25 21:58:51 -0300

    fix: corrigindo paginação na part2Service

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -14,7 +14,9 @@ namespace ProvaPub.Services
 
 		public ProductList  ListProducts(int page)
 		{
-			return new ProductList() {  HasNext=false, TotalCount =10, Products = _ctx.Products.ToList() };
+			int limit = page * 10;
+			int init = limit - 10;
+			return new ProductList() {  HasNext=false, TotalCount =10, Products = _ctx.Products.OrderBy(x => x.Id).Where(x => x.Id >= init && x.Id < limit).ToList() };
 		}
 
 	}
